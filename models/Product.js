b680f34675c6685821226b7663711f98e211c56c
patorma <pocontreras@ing.ucsc.cl>
2019-09-06 02:01:53 -0400

    Definicion de esquema de la coleccion

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+    //el objeto Schema nos sirve para crear los modelos
+const Schema = mongoose.Schema
+
+// Se ejecuta Schema como si fuera una funcion
+const ProductSchema = Schema({
+    name: String,
+    size: Number, //cantidad
+    unitaryPrice: Number,
+    imgUrl: String,
+    description
+}, {
+    //esto permite que nuestras colecciones se le agregue por deafaul una proipiedad createAt
+    //para mostrar cuando se creo y cuando se modifico
+    timestamps: true
+})
+
+//luego exportamos 
+//primer parametro como se llamara la coleccion y el schema que lo define
+module.exports = mongoose.model('Products', ProductSchema)
