5ab3b6463499fe6e90e6e2b89740280593b7a3da
patorma <pocontreras@ing.ucsc.cl>
2019-09-07 20:27:23 -0400

    se creo carpeta para almacenar imagenes

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const upload = require('../libs/storage')
 const { addProduct } = require('../controllers/productController')
 
 const api = express.Router()
 
 
 
-api.post('/products', addProduct)
+api.post('/products', upload.single('imgUrl'), addProduct)
 
 module.exports = api
